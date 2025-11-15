7160cc535b4c2c62a199723e3ab8b06c00eafed2
BenCaro8 <bccaro8@gmail.com>
2025-11-14 20:32:08 -0500

    Fixed Ethan's lack of formatting

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 let bingus = 'dingus';
-bingus =bingus + 1;
+bingus = bingus + 1;
 console.log(bingus);
 const truth = true;
 console.log(truth);
 
 console.log(myadd(6));
 
-function myadd(var1: number, var2?:number)
-{
+function myadd(var1: number, var2?: number) {
   return var1 + (var2 || 0);
-  
 }
